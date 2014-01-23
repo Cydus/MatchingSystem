a6c738347aef57146c9000f6c535d9bf3a360592
Alina <owner@ubuntu.(none)>
2014-01-23 21:09:31 +0000

    changed welcome message

--- matching_system_project/matching_system_project/views.py
+++ matching_system_project/matching_system_project/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to matching system")
+    return HttpResponse("Welcome to alinas website")
