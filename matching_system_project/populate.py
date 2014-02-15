16b0787630015cd91ed35636b6a06eb37f5fbbd7
Cydus <davidb1985@gmail.com>
2014-02-15 21:55:27 +0000

    more things to populate

--- matching_system_project/populate.py
+++ matching_system_project/populate.py
@@ -12,9 +12,44 @@ def populate():
                 expires="2014-03-15",
                 active=True)
 
-    add_project(title="Company Intranet",
-                description="We are building a new intranet that brings mangement and development under one roof",
-                created="2014-03-15",
+    add_project(title="Fiberoptic infrastructure",
+                description="New fiberoptics are important fot the future of the company, network engineers and architects are needed urgently for this overhaul.",
+                created="2013-03-16",
+                starts="2014-03-15",
+                expires="2014-03-15",
+                active=True)
+
+    add_project(title="Information retrieval research group",
+                description="The company is forming a group to research neural search. Lets build something awesome!",
+                created="2013-03-16",
+                starts="2014-03-15",
+                expires="2014-03-15",
+                active=True)
+
+    add_project(title="Cow Milking Robot 3.0",
+                description="The last cow milking robots were a huge disaster. University 2.0 must harness its bovine talent to suceed at this goal.",
+                created="2013-03-16",
+                starts="2014-03-15",
+                expires="2014-03-15",
+                active=True)
+
+    add_project(title="Rector election app",
+                description="Our students are lazy shits and don't vote enough for new Rectors. We are building a mobile app to get them involved.",
+                created="2013-03-16",
+                starts="2014-03-15",
+                expires="2014-03-15",
+                active=True)
+
+    add_project(title="Automatic Will Smith detection device",
+                description="Will Smith, formally known as the fresh price has been terrorizing campus students with awful films disguised as hollywood blockbusters. We need to manage this issue with a series of automatic turrets and will smith detection sensor arrays.",
+                created="2013-03-16",
+                starts="2014-03-15",
+                expires="2014-03-15",
+                active=True)
+
+    add_project(title="Coffee dispensing website",
+                description="I want a website that makes coffee for me. Don't tell me it isn't possible I've seen star trek.",
+                created="2013-03-16",
                 starts="2014-03-15",
                 expires="2014-03-15",
                 active=True)
